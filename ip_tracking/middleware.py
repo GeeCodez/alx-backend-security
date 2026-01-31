32fdee6723b8c181382fc14bd738eabced19b740
GeeCodez <godsway702@gmail.com>
2026-01-31 17:21:08 +0000

    Basic ip logging middleware

---
+++ ip_tracking/middleware.py
@@ -0,0 +1,17 @@
+from ipware import get_client_ip
+from .models import RequestLog
+
+class IPTrackingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        ip, _ = get_client_ip(request)
+        if ip:
+            RequestLog.objects.create(
+                ip_address=ip,
+                path=request.path
+            )
+
+        response = self.get_response(request)
+        return response
